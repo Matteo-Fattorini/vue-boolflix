ac5c8e74b67b76c7f187967b80391adf59a46a3d
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-12-03 13:13:01 +0100

    Added flags, interface, hover-description, stars

---
+++ js/data.js
@@ -0,0 +1,11 @@
+flags = {
+  EN: "css/img/USA.png",
+  JA: "css/img/JP.png",
+  KO: "css/img/KO.png",
+  ES: "css/img/ES.png",
+  FR: "css/img/FR.png",
+  IT: "css/img/IT.png",
+  HI: "css/img/HI.png",
+  ZH: "css/img/ZH.png",
+  NOTF: "css/img/white.png",
+};
--- js/main.js
+++ js/main.js
@@ -4,22 +4,46 @@ const app = new Vue({
     key: "375fb5d5a05f3c4fec8077369df2b91e",
     input: "",
     movies: [],
+    searchTitle: "Top-Rated",
+    flags,
   },
-  computed: {},
+
   methods: {
-    getQueryMovies(where) {
+    flagSource(language) {
+      language = language.toUpperCase();
+      if (language in this.flags) {
+        return this.flags[language];
+      } else {
+        console.log("ok");
+        return this.flags["NOTF"];
+      }
+    },
+
+    log(e) {
+      console.log("sono qui");
+    },
+
+    getQueryMovies(where, page = 1) {
       this.movies = [];
+      if (this.input == "") {
+        this.searchTitle = `i più popolari`.toUpperCase();
+      } else {
+        this.searchTitle = `Risultati per '${this.input}'`;
+      }
+
       axios
         .get("https://api.themoviedb.org/3" + where, {
-          params: { api_key: this.key, query: this.input },
+          params: { api_key: this.key, query: this.input, page: page },
         })
         .then((response) => {
           this.movies = response.data.results;
         })
         .catch((err) => {
           console.log("Attenzione" + err);
-        })
-        .finally(() => (this.input = ""));
+        });
+    },
+    voteTransform(vote) {
+      return Math.round(vote / 2 / 0.5) * 0.5;
     },
   },
 
